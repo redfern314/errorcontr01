a066df5b2f2647ba9ab1deafd1731af54b92727e
unknown <kgallagher@DFXYYN1.olin.edu>
2012-04-30 14:54:31 -0400

    added some comments etc

--- paritycheck.m
+++ paritycheck.m
@@ -1,5 +1,9 @@
-m = [1 1];
-G = [1 0 1 1 1;0 1 1 1 1];
-H = [1 1 1;1 1 1;1 0 0;0 1 0;0 0 1];
+%k = 2, length of message
+%n = 5, length of codeword
+
+m = [1 1]; 
+p = [1 1 1;1 1 1]; %k by (n-k) matrix
+G = [[1 0;0 1],p]; % k by k identity 
+H = [p;[1 0 0;0 1 0;0 0 1]]; %(n-k)by(n-k) identity matrix
 c = m*G
 c*H
