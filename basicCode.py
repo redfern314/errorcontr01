672137bc10410fa99bdc383102ee8be3ab908d20
hboortz <heather.boortz@students.olin.edu>
2012-05-06 15:41:25 -0400

    Put everything into functions and generally made code neater

--- basicCode.py
+++ basicCode.py
@@ -11,50 +11,76 @@ def binary(code):
 
 #Define message and generator matrix
 #Generate codeword
-c=mat([1,0]) #one letter codeword
-print "The original codeword is:"
-print c
-G=([1,0,1,1,1],[0,1,1,0,1])
-#add parity bit to end
-#print G
-c=binary(c*G)
-c[0,3]+=1 #introduce error
-c=binary(c)
+def getCodeword(c):
+    #c=mat([1,0]) #one letter codeword
+    #print "The original codeword is:"
+    #print c
+    G=([1,0,1,1,1],[0,1,1,0,1])
+    #add parity bit to end
+    #print G
+    c=binary(c*G)
+    c[0,3]+=1 #introduce error
+    c=binary(c)
+    return c
+
+def getH_T():
+    H_T=matrix([[1,1,1],[1,0,1],[1,0,0],[0,1,0],[0,0,1]])
+    return H_T
 
 #Create a dictionary with syndromes as keys and error vectors as values
-errors=matrix([[0,0,0,0,0],[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]])
-h_T=([1,1,1],[1,0,1],[1,0,0],[0,1,0],[0,0,1])
-syndromes=errors*h_T
-lookup={}
-for i in range(shape(errors)[1]):
-    strsyn=''
-    for j in range(shape(syndromes[i])[1]):
-        strsyn=strsyn+str(syndromes[i][0,j])
-    if strsyn in lookup:
-        print strsyn + ' is a duplicate syndrome - time to revise the code!'
-    lookup[strsyn]=errors[i]
+def getSyndromeTable():
+    errors=matrix([[0,0,0,0,0],[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]])
+    syndromes=errors*getH_T()
+    lookup={}
+    for i in range(shape(errors)[1]):
+        strsyn=''
+        for j in range(shape(syndromes[i])[1]):
+            strsyn=strsyn+str(syndromes[i][0,j])
+        if strsyn in lookup:
+            print strsyn + ' is a duplicate syndrome - time to revise the code!'
+        lookup[strsyn]=errors[i]
+    return lookup
 
 #Compute syndrome for received codeword
-print "The encoding is:"
-print c
-valid = c*h_T
-valid=binary(valid)
-print valid
-if valid.all()==0:
-    print "valid codeword"
-else:
-    print "an error has occured"
+def isValidCodeword(c):
+    #print "The encoding is:"
+    #print c
+    valid = c*getH_T()
+    valid=binary(valid)
+    #print valid
+    if valid.all()==0:
+        return True
+        #print "valid codeword"
+    else:
+        return False
+        #print "an error has occured"
 
 #Looks up the error vector
-errsyn=''
-for j in range(shape(valid)[1]):
-    errsyn=errsyn+str(valid[0,j])
-print errsyn
-if errsyn in lookup:
-    errvec=lookup[errsyn]
-    print 'Error vector: ',errvec
-else:
-    print 'not found'
-
+def getError(c):
+    errsyn=''
+    for j in range(shape(c*getH_T())[1]):
+        errsyn=errsyn+str((c*getH_T())[0,j])
+    print errsyn
+    lookup=getSyndromeTable()
+    if errsyn in lookup:
+        errvec=lookup[errsyn]
+        return errvec
+        print 'Error vector: ',errvec
+    else:
+        print 'not found'
+        return zeros(shape(c))
 #Corrects the error vector
-print errvec+c
+m=mat([1,0]) 
+print "The original message is:"
+print m
+c=getCodeword(m)
+print "The encoded message before errors is:"
+print c
+c[0,0]=0#introduce error
+print "The code after an error is introduced is:"
+print c
+e=getError(c)
+print "The error is:"
+print e
+print "The codeword after error correction is:"
+print e+c
