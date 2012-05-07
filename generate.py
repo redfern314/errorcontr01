d9045873d09cd56852175be4264cec5dd7ceae49
Derek Redfern <redfern.derek@gmail.com>
2012-05-06 20:15:32 -0400

    Finished the code for generating a random square LDPC matrix.

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 from numpy import *
+import pdb
 
 def generate_ldpc(rows,columns):
     #create a matrix of all zeros
@@ -13,17 +14,43 @@ def generate_ldpc(rows,columns):
                 randrow=int(random.random()*rows)
             ldpc[randrow,i]=1
 
+    numones=0.0
+    zeros_array = zeros((rows,1))
+    rowones = matrix(zeros_array)
     for i in range(rows):
-        numones=0
         for j in range(columns):
             if ldpc[i,j]==1:
                 numones+=1
-        while numones < 2:
-            randcol=int(random.random()*columns)
-            while ldpc[randcol,i]==1:
-                randcol=int(random.random()*columns)
-            ldpc[i,randcol]=1
+                rowones[i,0]+=1
+
     print ldpc
+    print rowones
+    avgones=numones/rows
+    if not int(avgones)==avgones:
+        print 'Avg number of ones: ' + str(avgones)
+        print 'This is invalid.'
+        pdb.set_trace()
+    
+    for a in range(10):
+        templist=[]
+        for i in range(rows):
+            if rowones[i,0]>avgones:
+                randcol=int(random.random()*columns)
+                while ldpc[i,randcol]==0:
+                    randcol=int(random.random()*columns)
+                for j in range(rows):
+                    if rowones[j,0]<avgones and (not ldpc[j,randcol]==1) and (not j==i):
+                        templist.append(j)
+                if not len(templist)==0:
+                    randrow=int(random.random()*len(templist))
+                    ldpc[i,randcol]=0
+                    rowones[i,0]-=1
+                    ldpc[templist[randrow],randcol]=1
+                    rowones[templist[randrow],0]+=1
+            templist=[]
+
+    print ldpc
+    print rowones
     return ldpc
 
-generate_ldpc(10,5)
+generate_ldpc(25,25)
